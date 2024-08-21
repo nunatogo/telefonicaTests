74fb1fcba2d9a5578e4b381c1e1d7399a4c63043
ntorogon <ntorogon@emeal.nttdata.com>
2024-08-21 17:14:33 -0500

    20240821 - Initial commit

---
+++ src/main/java/com/ejercicios/telefonicatest/TelefonicaTestApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicios.telefonicatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonicaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonicaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejercicios/telefonicatest/TelefonicaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicios.telefonicatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelefonicaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
